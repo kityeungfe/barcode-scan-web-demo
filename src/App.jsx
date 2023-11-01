5dd0da752e67fa064e77edade7855e15630b2c58
Kit Yeung <kit.yeung@cs.hkbn.com.hk>
2023-11-01 14:10:48 +0800

    feat: add file input

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,7 @@ function App() {
   return (
     <>
       <div>
+        <input type="file" accept="image/*" capture="camera"/>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
